8b520534f219bcd6517e5adfd11ef6097aff15db
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2017-01-01 14:06:29 +0000

    Make command reliably execute; change return to \r (works better).

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 'use strict';
 
+let fs = require('fs');
+let loaded = false;
+
+// debugging purposes
+// function log(str) {
+// 	fs.appendFile(`${__dirname}/log.txt`, str + '\n');
+// }
+
+function waitForSessions(sessions) {
+	if (!loaded) {
+		sessions.forEach(session => {
+			session.write('echo hello world');
+			session.write('\r');
+			loaded = true;
+		});
+
+		setTimeout(() => waitForSessions(sessions), 10);
+	}
+}
 
 exports.onWindow = function (win) {
-	setTimeout(() => {
-        win.sessions.forEach(session => {
-            session.write('echo hello world');
-            session.write('\x0a');
-        })
-    }, 1000)
+	waitForSessions(win.sessions);
 };
-
-

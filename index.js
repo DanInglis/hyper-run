85e9438b821205448e092a7026a7e45b697ab64e
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2017-01-01 14:45:08 +0000

    Add configuration value to change commands

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 let fs = require('fs');
 let loaded = false;
+let commands = [];
 
 // debugging purposes
 // function log(str) {
@@ -11,14 +12,22 @@ let loaded = false;
 function waitForSessions(sessions) {
 	if (!loaded) {
 		sessions.forEach(session => {
-			session.write('echo hello world');
-			session.write('\r');
+			commands.forEach(cmd => {
+				session.write(cmd + '\r');
+			});
 			loaded = true;
 		});
 
 		setTimeout(() => waitForSessions(sessions), 10);
 	}
 }
+
+exports.onApp = function (obj) {
+	const config = obj.config.getConfig();
+	if (config.commands) {
+		commands = config.commands;
+	}
+};
 
 exports.onWindow = function (win) {
 	waitForSessions(win.sessions);

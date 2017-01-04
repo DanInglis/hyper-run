16b16e80ac2660c4a16584bca714b8ff29acfa27
Dan <dan@Dans-MacBook-Pro.local>
2017-01-03 19:15:56 -0500

    Tried using the methods from cwd

--- index.js
+++ index.js
@@ -1,39 +1,123 @@
 'use strict';
+///
 
+const { exec } = require('child_process');
+
+let curPid;
+let uids = {};
+
+let cmdType = 0;
+
+
+const setCwd = (pid) =>
+  exec(`lsof -p ${pid} | grep cwd | tr -s ' ' | cut -d ' ' -f9-`, (err, cwd) => {
+    if (err) {
+      console.error(err);
+    } else {
+      cwd = cwd.trim();
+      store.dispatch({
+        type: 'SESSION_SET_CWD',
+        cwd
+      });
+    }
+  });
+
+exports.middleware = (store) => (next) => (action) => {
+	cmdType = 0;
+	console.log(action);
+  switch (action.type) {
+    case 'SESSION_PTY_DATA':
+      if (curPid && uids[action.uid] === curPid) setCwd(curPid);
+      break;
+    case 'SESSION_ADD':
+      uids[action.uid] = action.pid;
+      curPid = action.pid;
+      setCwd(curPid);
+
+      cmdType = 1;	//new tab
+      break;
+    case 'SESSION_SET_ACTIVE':
+      curPid = uids[action.uid];
+      setCwd(curPid);
+      break;
+    case 'SESSION_PTY_EXIT':
+      delete uids[action.uid];
+      break;
+    case 'SESSION_USER_EXIT':
+      delete uids[action.uid];
+      break;
+  }
+  next(action);
+};
+
+
+
+
+////
 let fs = require('fs');
 let loaded = false;
 let cmdDefault = [];	//winType = 0
 let cmdTab = [];		//winType = 1
 //let cmdWindow = [];		//winType = 2
 
-let exists;
 
-//Gets called on change to the terminal
-exports.middleware = (store) => (next) => (action) => {
-	console.log(exists);	//outputs false
-	console.log(cmdDefault);
-	next(action);
-};
+//let cmdType = 1;
 
 
-function waitForSessions(sessions, commands) {
-	if (!loaded) {
+function output(session) {
+
+	cmdDefault.forEach(cmd => {
+			session.write(cmd + '\r');
+		});
+
+	/*if(!loaded){
+		cmdDefault.forEach(cmd => {
+			session.write(cmd + '\r');
+		});
+		loaded = true;
+	}
+	
+	setTimeout(() => output(session), 10);
+
+		/*switch(cmdType){
+				case 0:
+				cmdDefault.forEach(cmd => {
+					session.write(cmd + '\r');
+				});
+				break;
+				case 1:
+				cmdTab.forEach(cmd => {
+					session.write(cmd + '\r');
+				});
+				break;
+			};*/
+
+	/*if (!loaded) {
 		sessions.forEach(session => {
-			commands.forEach(cmd => {
-				session.write(cmd + '\r');
-			});
+			switch(cmdType){
+				case 0:
+				cmdDefault.forEach(cmd => {
+					session.write(cmd + '\r');
+				});
+				break;
+				case 1:
+				cmdTab.forEach(cmd => {
+					session.write(cmd + '\r');
+				});
+				break;
+			}
+
 			loaded = true;
 		});
 
-		setTimeout(() => waitForSessions(sessions, commands), 10);
-	}
+		setTimeout(() => output(sessions), 10);
+	}*/
 };
 
-//gets called on app startup
 exports.onApp = function (obj) {
 	const config = obj.config.getConfig();
 	if (config.hyperRun) {
-		cmdDefault = config.hyperRun
+		cmdDefault = config.hyperRun;
 	}
 	if (config.hyperRunTab){
 		cmdTab = config.hyperRunTab;
@@ -41,5 +125,18 @@ exports.onApp = function (obj) {
 };
 
 exports.onWindow = function (win) {
-	waitForSessions(win.sessions, cmdDefault);
+	//write(win.sessions);
+	//output(win.sessions);
+
+	/*setTimeout(() => {
+        win.sessions.forEach(session => {
+            output(session);
+        })
+    }, 1000)*/
+
+	setTimeout(() => {
+		output(win.sessions.get(curPid));
+	}, 1000);
+
 };
+
